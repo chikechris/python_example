cb204164424a037dd6103b718f47d97e60f9b4a1
chikechris <cchike2017@gmail.com>
2019-12-02 14:00:04 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+
+
+import os
+os.system('clear')
+full_name = "christian ubani"
+full_name = "chris ubani"
+age = 41  # number data types
+names = ["john", "Mike", "Mary"]
+print("hi")
+# printing here i come to the screen this is the programatic
+print(full_name)
+print(names)
