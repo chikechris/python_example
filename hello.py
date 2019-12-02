4c389c9173638b7273bc88363a3286677f32d86b
chikechris <cchike2017@gmail.com>
2019-12-02 14:09:29 -0500

    second commit

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-
-
+'''
+Learning Python
+'''
 import os
 os.system('clear')
 full_name = "christian ubani"
 full_name = "chris ubani"
-age = 41  # number data types
+age = 0  # number data types
 names = ["john", "Mike", "Mary"]
 print("hi")
 # printing here i come to the screen this is the programatic
